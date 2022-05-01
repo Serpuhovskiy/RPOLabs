960364ac1f7a0536e1d8c48efa98be165a34710e
Serpuhovskiy <99271524+Serpuhovskiy@users.noreply.github.com>
2022-05-01 14:45:50 +0300

    labs 5-7 (1)

---
+++ backend/src/main/java/ru/iu3/backend/controllers/UserController.java
@@ -0,0 +1,145 @@
+package ru.iu3.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.iu3.backend.models.Museum;
+import ru.iu3.backend.models.User;
+import ru.iu3.backend.repositories.MuseumRepository;
+import ru.iu3.backend.repositories.UserRepository;
+
+
+import java.util.*;
+
+@RestController
+@RequestMapping("api/v1")
+public class UserController {
+    // По аналогии здесь используется два репозитория
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    MuseumRepository museumRepository;
+
+
+    @GetMapping("/users")
+    public List getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUsers(@RequestBody User user) throws Exception {
+        try {
+            User nc = userRepository.save(user);
+            return new ResponseEntity<Object>(nc, HttpStatus.OK);
+        } catch (Exception exception) {
+            // Указываем тип ошибки
+            String error;
+            if (exception.getMessage().contains("ConstraintViolationException")) {
+                error = "artistAlreadyExists";
+            } else {
+                error = exception.getMessage();
+            }
+
+            Map<String, String> map = new HashMap<>();
+            map.put("error", error + "\n");
+
+            return ResponseEntity.ok(map);
+        }
+    }
+
+
+    @PostMapping("/users/{id}/addmuseums")
+    public ResponseEntity<Object> addMuseums(@PathVariable(value = "id") Long userID,
+                                             @Validated @RequestBody Set<Museum> museums) {
+        // Извлекаем пользователя по конкретному ID-шнику
+        Optional<User> uu = userRepository.findById(userID);
+        int cnt = 0;
+
+        if (uu.isPresent()) {
+            User u = uu.get();
+
+            // Если музеев несколько (а такое может быть вполне, то тогда добавляем их поочерёдно)
+            for(Museum m: museums) {
+                // Если есть музей, то мы, конечно, добавим его. Защита от дурака
+                Optional<Museum> mm = museumRepository.findById(m.id);
+                if (mm.isPresent()) {
+                    u.addMuseum(mm.get());
+                    ++cnt;
+                }
+            }
+
+            // Сохраняем
+            userRepository.save(u);
+        }
+
+        // Формируем
+        Map<String, String> response = new HashMap<>();
+        response.put("added", String.valueOf(cnt));
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping("/users/{id}/removemuseums")
+    public ResponseEntity<Object> removeMuseums(@PathVariable(value = "id") Long userId,
+                                                @Validated @RequestBody Set<Museum> museums) {
+        Optional<User> uu = userRepository.findById(userId);
+        int cnt = 0;
+
+        if (uu.isPresent()) {
+            User u = uu.get();
+            for (Museum m: museums) {
+                u.removeMuseum(m);
+                ++cnt;
+            }
+
+            userRepository.save(u);
+        }
+
+        // Формируем ответ
+        Map<String, String> response = new HashMap<>();
+        response.put("count", String.valueOf(cnt));
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUsers(@PathVariable(value = "id") Long userId,
+                                             @RequestBody User userDetails) {
+        User user = null;
+        Optional<User> uu = userRepository.findById(userId);
+        if (uu.isPresent()) {
+            // Заполняем пользовательские данные
+            user = uu.get();
+            user.login = userDetails.login;
+            user.email = userDetails.email;
+
+            userRepository.save(user);
+            return ResponseEntity.ok(user);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+        }
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Object> deleteUsers(@PathVariable(value = "id") Long userId) {
+        Optional<User> users = userRepository.findById(userId);
+        Map<String, Boolean> resp = new HashMap<>();
+
+        // Возвратит true, если объект существует (не пустой)
+        if (users.isPresent()) {
+            userRepository.delete(users.get());
+            resp.put("deleted", Boolean.TRUE);
+        } else {
+            resp.put("deleted", Boolean.FALSE);
+        }
+
+        return ResponseEntity.ok(resp);
+    }
+}
